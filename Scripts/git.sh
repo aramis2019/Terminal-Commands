338dc0800ad6a7c090db70e487f6f201688c5b89
protasovd <dmitry.protasov@broadridge.com>
2023-12-21 15:37:11 +0000

    gitlab

---
+++ Scripts/git.sh
@@ -0,0 +1,22 @@
+git config --global http.sslVerify false
+
+git status
+
+
+
+mkdir my-cool-website
+cd people
+git init
+
+
+
+
+git clone https://gitlab.com/Aramis2019/testproject1.git
+
+#####################################################
+gitlab.com
+protassov@gmail.com
+Token name:gitlab-21-12-2023
+Your new personal access token: glpat-x1XTy9ZB_bTgV6fzGwHc
+Expires: 17-12-2024
+#####################################################
